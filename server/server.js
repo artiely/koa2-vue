a93d65e0495a3a496463b08a222520ff6e5f463c
artiely <1119696785@qq.com>
2018-03-07 16:44:34 +0800

    加入koa-static

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import {join,resolve} from 'path'
 import Koa from 'koa'
-// const serve = require('koa-static')
+const serve = require('koa-static')
 import R from 'ramda'
 import chalk from 'chalk'
 import config from './config'
@@ -28,7 +28,7 @@ async function start() {
   //    app.use(cors())
   // }
   //  app.use(cors())
-  // app.use(serve(resolve(__dirname , '../pc/dist')))
+  app.use(serve(resolve(__dirname , '../pc/dist')))
   await useMiddlewares(app)
   //等价于下面
   // database(app)
