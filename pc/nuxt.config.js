df73dbf412a53c0b4339577cda5a246e350cb6d4
artiely <1119696785@qq.com>
2018-03-09 11:11:58 +0800

    修改nuxt配置

--- pc/nuxt.config.js
+++ pc/nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
       {hid: 'description', name: 'description', content: 'Nuxt.js project'},
     ],
-    script: [{src: '~assets/isMobile.js'}],
+    script: [{src: '/static/isMobile.js'}],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
       {
