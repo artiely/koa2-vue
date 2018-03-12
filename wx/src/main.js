e4c27fde11b3d691f9059053a54f71a163ed74b8
artiely <1119696785@qq.com>
2018-03-12 17:47:37 +0800

    请求数据

--- wx/src/main.js
+++ wx/src/main.js
@@ -10,12 +10,37 @@ app.$mount()
 export default {
   // 这个字段走 app.json
   config: {
-    pages: ['pages/logs/logs', '^pages/index/index', 'pages/counter/counter'], // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
+    pages: ['^pages/index/index', 'pages/counter/counter', 'pages/logs/logs'], // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
       navigationBarTitleText: 'WeChat',
       navigationBarTextStyle: 'black'
+    },
+    tabBar: {
+      color: '#a9b7b7',
+      selectedColor: '#11cd6e',
+      borderStyle: 'white',
+      list: [
+        {
+          selectedIconPath: 'static/img/1.png',
+          iconPath: 'static/img/1.png',
+          pagePath: 'pages/index/index',
+          text: '文章'
+        },
+        {
+          selectedIconPath: 'static/img/2.png',
+          iconPath: 'static/img/2.png',
+          pagePath: 'pages/counter/counter',
+          text: '电影'
+        },
+        {
+          selectedIconPath: 'static/img/3.png',
+          iconPath: 'static/img/3.png',
+          pagePath: 'pages/logs/logs',
+          text: '福利'
+        }
+      ]
     }
   }
 }
--- wx/src/pages/index/index.vue
+++ wx/src/pages/index/index.vue
@@ -1,19 +1,5 @@
 <template>
   <div class="container" @click="clickHandle('test click', $event)">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-      
     <form class="form-container">
       <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
       <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
@@ -24,23 +10,24 @@
 
 <script>
 import card from '@/components/card'
-
+// import axios from 'axios'
 export default {
   data () {
     return {
       motto: 'Hello World',
-      userInfo: {}
+      userInfo: {},
+      list: null
     }
   },
-
   components: {
     card
   },
-
   methods: {
     bindViewTap () {
       const url = '../logs/logs'
-      wx.navigateTo({ url })
+      wx.navigateTo({
+        url
+      })
     },
     getUserInfo () {
       // 调用登录接口
@@ -56,12 +43,22 @@ export default {
     },
     clickHandle (msg, ev) {
       console.log('clickHandle:', msg, ev)
+    },
+    getData () {
+      wx.request({
+        url: 'http://08tj.com/api/v0/juejin/1/10',
+        // data:{},
+        header: {'Content-Type': 'application/json'},
+        success: function (res) {
+          console.log(res)
+        }
+      })
     }
   },
-
   created () {
     // 调用应用实例的方法获取全局数据
     this.getUserInfo()
+    this.getData()
   }
 }
 </script>
@@ -72,22 +69,18 @@ export default {
   flex-direction: column;
   align-items: center;
 }
-
 .userinfo-avatar {
   width: 128rpx;
   height: 128rpx;
   margin: 20rpx;
   border-radius: 50%;
 }
-
 .userinfo-nickname {
   color: #aaa;
 }
-
 .usermotto {
   margin-top: 150px;
 }
-
 .form-control {
   display: block;
   padding: 0 12px;
